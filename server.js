4ee94357a95fd3f718cd235592d2e956a0c27f2a
PauloSantosJr <61476763+PauloSantosJr@users.noreply.github.com>
2020-03-22 18:25:38 -0300

    criado uma nova rota 'Player'

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ const app = express();
 
 var port = process.env.PORT || 80
 
+console.log(__dirname);
+
 app.get('/', (req, res) => {
     res.send('Olá Mundo!');
+
+});
+
+app.get('/player', (req, res) => {
+    res.send('Olá Player!');
 
 });
 
